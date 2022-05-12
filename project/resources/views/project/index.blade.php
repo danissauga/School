9a91d896cd4106c307b9315eb00f2734c7d2e0f6
ingabesus <93394843+ingabesus@users.noreply.github.com>
2022-05-12 11:35:08 +0300

    Update

--- project/resources/views/project/index.blade.php
+++ project/resources/views/project/index.blade.php
@@ -25,7 +25,7 @@
             <td>{{$project->title}}</td>
             <td>{{$project->groups_number}}</td>
             <td>{{$project->students_number}} </td>
-            <!-- <td><a class="btn btn-primary" href="{{route('project.edit', [$project])}}">Edit</a></td> -->
+            <td><a class="btn btn-primary" href="{{route('project.edit', [$project])}}">Edit</a></td> 
             <td><a class="btn btn-secondary" href="{{route('project.show', [$project])}}">Show</a></td>
             <td>
                 <form method="post" action="{{route('project.destroy', [$project])}}">
