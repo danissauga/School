2f242cc701a88b284b9065409e73f982c18415ce
ingabesus <93394843+ingabesus@users.noreply.github.com>
2022-05-12 10:58:50 +0300

    Navigation

--- project/resources/views/student/index.blade.php
+++ project/resources/views/student/index.blade.php
@@ -28,58 +28,9 @@
             @endforeach
     </table>
 
-Asign students to the group
-           
-Group:  <select class="form-select" name="group_id">
-                @foreach ($groups as $group)
-                        <option value="{{ $group->id }}">{{ $group->title }}</option>
-                @endforeach
-            </select>
+<a class="btn btn-primary" href="{{route('attendancegroup.index') }}">Asign students to the group </a>    
 
 </div>
-       <script>
-
-           $(document).ready(function(){
-               console.log ('jquery veikia');
-           })
-
-           let csrf = '123456789';
-
-           function createRowFromHtml(studentId, studentName, studentSurname, studentEmail) {
-               $(".template tr").removeAttr("class");
-               $(".template tr").addClass("student"+studentId);
-               $(".template .delete-student").attr('data-studentid', studentId );
-               $(".template .show-student").attr('data-studentid', studentId );
-               $(".template .edit-student").attr('data-studentid', studentId );
-               $(".template .col-student-id").html(studentId );
-               $(".template .col-student-name").html(studentName );
-               $(".template .col-student-surname").html(studentSurname );
-               $(".template .col-student-email").html(studentEmail );
-              // $(".template .col-student-group").html( studentGroupId);
-           
-               return $(".template tbody").html();
-               }
-
-               $.ajax({
-                   type: 'GET',// formoje method POST GET
-                   url: 'http://127.0.0.1:8000/api/students' ,// formoje action
-                   data: {csrf:csrf},
-                   success: function(data) {
-                       $('#students tbody').html('');
-                       $.each(data, function(key, student){
-                           let html;
-                           
-                           html = createRowFromHtml(student.id, student.name, student.surname, student.email);
-                           $("#students tbody").append(html);
-
-                       });
-                       console.log(data);
-                       
-
-                   }
-               });
-           
-
-       </script>
+     
 
 @endsection
